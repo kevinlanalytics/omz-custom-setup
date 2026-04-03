1d7bfcfe7e4773261382da06b65d279833f00a61
Kevin Lan <kevin.lan@cercle.ai>
2026-04-03 16:02:47 -0400

    update on R_commonpackages.R script for renv library

--- R_common_packages.R
+++ R_common_packages.R
@@ -1,9 +1,67 @@
-common_packages <- c(
+common_cran_packages <- c(
   "tidyverse",
   "data.table",
-  "readr",
+  "brio",
+  "cachem",
+  "cli",
+  "desc",
+  "devtools",
+  "digest",
+  "dplyr",
+  "ellipsis",
+  "farver",
+  "fastmap",
+  "fs",
+  "generics",
   "ggplot2",
-  "devtools"
+  "glue",
+  "gtable",
+  "hms",
+  "htmltools",
+  "htmlwidgets",
+  "httpuv",
+  "later",
+  "lifecycle",
+  "magrittr",
+  "memoise",
+  "mime",
+  "miniUI",
+  "patchwork",
+  "pillar",
+  "pkgbuild",
+  "pkgconfig",
+  "pkgload",
+  "profvis",
+  "promises",
+  "purrr",
+  "R6",
+  "RColorBrewer",
+  "Rcpp",
+  "readr",
+  "remotes",
+  "rlang",
+  "rprojroot",
+  "rstudioapi",
+  "S7",
+  "scales",
+  "sessioninfo",
+  "shiny",
+  "testthat",
+  "tibble",
+  "tidyr",
+  "tidyselect",
+  "tzdb",
+  "urlchecker",
+  "usethis",
+  "vctrs",
+  "withr",
+  "xtable",
+  "installr"
+)
+
+common_github_packages <- c(
+  "stufield/helpr",
+  "stufield/wranglr"
 )
 
 if (!requireNamespace("renv", quietly = TRUE)) {
@@ -14,6 +72,7 @@ if (!requireNamespace("renv", quietly = TRUE)) {
   stop("The 'renv' package is required but could not be installed.")
 }
 
-renv::install(common_packages)
+renv::install(common_cran_packages)
+renv::install(common_github_packages)
 renv::snapshot()
 
